08b44b8cd68a7623b2843f970bb4477ac46fe508
gehurley22 <gehurley@umich.edu>
2020-04-20 18:43:47 -0400

    Made data base for pollution

--- Pollution.py
+++ Pollution.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import os
+import sqlite3
 
 baseurl = "https://www.numbeo.com"
 r = requests.get("https://www.numbeo.com/pollution/rankings_by_country.jsp?title=2020&displayColumn=0")
@@ -17,7 +18,7 @@ for country in Countrynames:
 
 Pollution2020 = []
 
-Pollution = soup.find_all("td", class_ = "sorting_1")
+Pollution = soup.find_all("td", style = "text-align: right")
 for number in Pollution:
     number = number.text
     Pollution2020.append(number)
@@ -30,16 +31,53 @@ Countrynames2019 = []
 Countrynames2 = soup.find_all("td", class_ = "cityOrCountryInIndicesTable")
 for country1 in Countrynames2:
     country1 = country1.text
-    if country1 in Countrynames2020:
-        Countrynames2019.append(country1)
+    Countrynames2019.append(country1)
 #print(Countrynames2019)
 
 Pollution2019 = []
-Pollution2 = soup.find_all("td", style = "text-align: right", class_ = "sorting_1")
+Pollution2 = soup.find_all("td", style = "text-align: right")
 for num in Pollution2:
     num = num.text
     Pollution2019.append(num)
 #print(Pollution2019)
+
+tup2019 = []
+for x in range(len(Countrynames2019)):
+    tup = (Countrynames2019[x],Pollution2019[x])
+    tup2019.append(tup)
+
+tup2020 =[]
+for x in range(len(Countrynames2020)):
+    if Countrynames2020[x] in Countrynames2019:
+        tup = (Countrynames2020[x],Pollution2020[x])
+        tup2020.append(tup)
+
+
+#print(sorted(tup2020))
+#print(sorted(tup2019))
+
+tup2019 = sorted(tup2019)
+tup2020 = sorted(tup2020)
+
+conn = sqlite3.connect("pollutiondb")
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS countryid (id INTEGER PRIMARY KEY, country STRING)")
+for i in range(len(tup2019)):
+    cur.execute("INSERT INTO countryid (id,country) VALUES (?,?)",(i,tup2019[i][0]))
+conn.commit()
+cur.execute('DROP TABLE IF EXISTS pollution2019')
+cur.execute('''CREATE TABLE pollution2019 (id INTEGER PRIMARY KEY, pollutionindex FLOAT) ''')
+for i in range(len(tup2019)):
+    cur.execute("INSERT INTO pollution2019 (id,pollutionindex) VALUES (?,?)",(i,tup2019[i][1]))
+conn.commit()
+
+cur.execute('DROP TABLE IF EXISTS pollution2020')
+cur.execute('''CREATE TABLE pollution2020 (id INTEGER PRIMARY KEY, pollutionindex FLOAT) ''')
+for i in range(len(tup2020)):
+    cur.execute("INSERT INTO pollution2020 (id,pollutionindex) VALUES (?,?)",(i,tup2020[i][1]))
+conn.commit()
+
+
 
 
 
