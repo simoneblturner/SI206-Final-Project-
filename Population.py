28f2b6cad98991bcd60c21da190dd039357e151b
Simone Turner <sbturner@umich.edu>
2020-04-24 20:05:43 -0500

    starting on new tables

--- Population.py
+++ Population.py
@@ -3,14 +3,14 @@ import requests
 import re
 import os
 import sqlite3
-import Pollution 
+#import join
+
+#PART 1 -- Get List of Countries from Funcation 
 
 def country_list_population():
-    
-    pollution_countries = Pollution.country_list()
 
     country_list_population = []
-    # requesting information on country by population from API
+
     url = "https://ajayakv-rest-countries-v1.p.rapidapi.com/rest/v1/all"
 
     headers = {
@@ -20,16 +20,35 @@ def country_list_population():
 
     response = requests.request("GET", url, headers=headers)
 
-    #turning population data into JSON format 
     data = response.json()
+
     for country in data:
         country_list_population.append(country["name"])
+    
+    return country_list_population
 
-    #creating population database 
-    conn = sqlite3.connect('Popul-density.db')
-    cur = conn.cursor()
 
-    #creating population table 
+#PART 2 
+#Adding to database created in join.py
+
+# requesting information on country by population from API
+url = "https://ajayakv-rest-countries-v1.p.rapidapi.com/rest/v1/all"
+
+headers = {
+    'x-rapidapi-host': "ajayakv-rest-countries-v1.p.rapidapi.com",
+    'x-rapidapi-key': "02e0a7d154mshbc0feed6cd14574p180afajsn055d7cf35fd7"
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+#turning population data into JSON format 
+data = response.json()
+
+#taking database from joint file
+join.create_database()
+
+def popultion_data():
+
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS popul_density(
@@ -49,24 +68,20 @@ def country_list_population():
     country_id = 0 
     #inserting data into table
     for country in data:
-        if country["name"] in pollution_countries:
-            try:
-                if index < 100: 
-                    density = country["population"] / country["area"]
-                    cur.execute(
-                        """
-                        INSERT INTO popul_density(id, country, population, area, density)
-                        VALUES (?, ?, ?, ?, ?)
-                        """,
-                        (country_id, country['name'], country['population'], country["area"], density))
-                    index += 1 
-                    country_id += 1 
-            except:
-                continue 
+        try:
+            if index < 100: 
+                density = country["population"] / country["area"]
+                cur.execute(
+                    """
+                    INSERT INTO popul_density(id, country, population, area, density)
+                    VALUES (?, ?, ?, ?, ?)
+                    """,
+                    (country_id, country['name'], country['population'], country["area"], density))
+                index += 1 
+                country_id += 1 
+        except:
+            continue 
 
     #commit table 
     conn.commit()
-    
-    return country_list_population
 
-population = country_list_population()
