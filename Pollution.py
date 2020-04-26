94a28936635d232ed63f68d7b9ab57169b48cc6c
Simone Turner <sbturner@umich.edu>
2020-04-25 19:57:29 -0500

    newwww

--- Pollution.py
+++ Pollution.py
@@ -32,17 +32,12 @@ def country_list_pollution():
     for country1 in Countrynames2:
         country1 = country1.text
         Countrynames2019.append(country1)
-    
-    return Countrynames2019
-    country = country_list_pollution()
 
     Pollution2019 = []
     Pollution2 = soup.find_all("td", style = "text-align: right")
     for num in Pollution2:
         num = num.text
         Pollution2019.append(num)
-#print(Pollution2019)
-   
 
     tup2019 = []
     for x in range(len(Countrynames2019)):
@@ -55,40 +50,7 @@ def country_list_pollution():
             tup = (Countrynames2020[x],Pollution2020[x])
             tup2020.append(tup)
 
-
-    #print(len(tup2020))
-    #print(len(tup2019))
-
     tup2019 = sorted(tup2019)
     tup2020 = sorted(tup2020)
 
-    conn = sqlite3.connect("pollutiondb.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS countryid (id INTEGER PRIMARY KEY, country STRING)")
-    for i in range(len(tup2019)):
-        cur.execute("INSERT INTO countryid (id,country) VALUES (?)",(tup2019[i][0]))
-    conn.commit()
-    cur.execute('DROP TABLE IF EXISTS pollution2019')
-    cur.execute('''CREATE TABLE pollution2019 (id INTEGER PRIMARY KEY, pollutionindex FLOAT) ''')
-    for i in range(len(tup2019)):
-        cur.execute("INSERT INTO pollution2019 (id,pollutionindex) VALUES (?,?)",(i,tup2019[i][1]))
-    conn.commit()
-
-    cur.execute('DROP TABLE IF EXISTS pollution2020')
-    cur.execute('''CREATE TABLE pollution2020 (id INTEGER PRIMARY KEY, pollutionindex FLOAT) ''')
-    for i in range(len(tup2020)):
-        cur.execute("INSERT INTO pollution2020 (id,pollutionindex) VALUES (?,?)",(i,tup2020[i][1]))
-    conn.commit()
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return Countrynames2019
