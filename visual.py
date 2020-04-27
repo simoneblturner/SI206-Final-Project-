06147324fffb99acf21f76b61c50e8fc4cfc39e0
Simone Turner <sbturner@umich.edu>
2020-04-26 22:36:02 -0500

    done

--- visual.py
+++ visual.py
@@ -7,15 +7,14 @@ import sqlite3
 import matplotlib
 import matplotlib.pyplot as plt
 
+# CONNECT TO THE COVID_Pollution_Correlation.db
 conn = sqlite3.connect("COVID_Pollution_Correlation.db")
 cur = conn.cursor()
 
-# JOIN CASES DATA AND POPULATION DENSITY DATA
-
+# FUNCTION TO JOIN CASES DATA AND POPULATION DENSITY DATA
 def cases_density():
 
-    print("\n Density vs Cases \n")
-
+    # JOIN TABLES 
     cur.execute(
     '''
     SELECT Country_ids.country_name, popul_density.density, Cases.cases 
@@ -30,6 +29,7 @@ def cases_density():
     conn.commit()
     all = cur.fetchall()
 
+    # CREATE LIST OF TUPLES 
     tups = []
     for i in all: 
         tups.append(i)
@@ -39,6 +39,7 @@ def cases_density():
     density = []
     cases = []
     
+    # TURN INTO FLOATS
     for tup in tups:
         names.append(tup[0])
         density.append(tup[1])
@@ -49,20 +50,19 @@ def cases_density():
         else:
             cases.append(float(stri))
    
-    plt.figure(1, figsize = (9,3))
-    fig, ax = plt.subplots()
-    ax.scatter(density, cases)
-    ax.set_xlabel("Population Density")
+    # CREATE VISUALIXATION
+    fig, ax = plt.subplots() 
+    ax.scatter(x = density, y = cases, color="red") 
+    ax.set_xlabel("Population Density by Country")
     ax.set_ylabel("COVID-19 Cases")
-    ax.set_title("Population Density by County vs COVID-19 Cases")
+    ax.set_title("Population Density by Country vs COVID-19 Cases")
     plt.savefig("DENSITY_CASES.png")
     plt.show()
 
-# JOIN CASES DATA AND POLLUTION 2019 DATA
+# FUNCRION TO JOIN CASES DATA AND POLLUTION 2019 DATA
 def difference_in_pollution():
-
-    print("\n Difference in Pollution 2019/2020\n")
     
+    # JOIN TABLES 
     cur.execute(
     '''
     SELECT Country_ids.country_name, pollution2019.pollution_index, 
@@ -75,11 +75,14 @@ def difference_in_pollution():
     '''
     )
     conn.commit()
+
+    # CREATE LIST OF TUPLES
     all = cur.fetchall()
     tups = []
     for i in all: 
         tups.append(i)
    
+    # CREATE TEXT FILE WITH CALCULATION 
     with open("pollution_difference.txt", "w") as output:
         output.write("Country Name, Pollution 2019, Pollution 2020, Pollution Difference\n")
         for tup in tups:
@@ -90,10 +93,8 @@ def difference_in_pollution():
 
             output.write(str(stri) + "\n")
 
-
+# FUNCTION TO JOIN AND CREATE VISULIZATION FOR CASES AND DIFFERENCE IN POLLUTION BETWEEN 2019-2020
 def cases_difference():
-
-    print("\nCases vs Pollution 2019 \n")
 
     cur.execute(
     '''
@@ -110,6 +111,8 @@ def cases_difference():
     '''
     )
     conn.commit()
+
+    # PUT DATA INTO LIST OF TUPLES
     tups = []
     all = cur.fetchall()
     for i in all: 
@@ -119,6 +122,7 @@ def cases_difference():
     cases = []
     cases_nums = []
     
+    # CONVERT STRINGS TO FLOATS 
     for tup in tups:
         difference.append(tup[4])
         cases.append(tup[1])
@@ -128,18 +132,20 @@ def cases_difference():
             num = float(case.replace(",", ""))
         cases_nums.append(num)
 
-    plt.figure(1, figsize = (9, 3))
+    # CREATE VISUALIZATION 
+    #plt.figure(2, figsize = (8, 8))
     fig, ax = plt.subplots()
-    ax.scatter(cases_nums, difference)
+    ax.scatter(cases_nums, difference, color="green")
     ax.set_xlabel("COVID-19 Cases")
     ax.set_ylabel("Change in Pollution")
     ax.set_title("COVID-19 Cases vs Change in Pollution")
     plt.savefig("DIFFERENCE_CASES.png")
     plt.show()
 
-
+# FUNCTION TO JOIN AND CREATE VISUAL FOR POLLUTION DIFFERENCE AND POPULATION DENSITY 
 def difference_density():
     
+    # JOIN TABLES 
     cur.execute(
     '''
     SELECT Country_ids.country_name, popul_density.density, pollution2019.pollution_index, pollution2020.pollution_index,
@@ -157,6 +163,7 @@ def difference_density():
     conn.commit()
     all = cur.fetchall()
 
+    # CREATE LIST OF TUPLES 
     tups = []
     for i in all: 
         tups.append(i)
@@ -168,16 +175,78 @@ def difference_density():
         difference.append(tup[4])
         density.append(tup[1])
 
-    plt.figure(1, figsize = (9,3))
+    # CREATE VISUALIZATION 
+    #plt.figure(3, figsize = (8,8))
     fig, ax = plt.subplots()
-    ax.scatter(density, difference)
+    ax.scatter(density, difference, color="orange")
     ax.set_xlabel("Population Density")
     ax.set_ylabel("Change In Pollution")
     ax.set_title("Population Density by Change in Pollution")
     plt.savefig("DENSITY_DIFFERENCE.png")
     plt.show()
 
-difference_in_pollution()
-cases_density()
-cases_difference()
-difference_density()
+def rate_cases_deaths():
+    cur.execute(
+    '''
+    SELECT Cases.country_name, Cases.cases, Cases.deaths
+    FROM Cases
+    '''
+    )
+    conn.commit()
+    all = cur.fetchall()
+
+    tups = []
+    for i in all: 
+        tups.append(i)
+    
+    cases = []
+    deaths = []
+    countries = []
+
+    num_cases = []
+    num_deaths = []
+
+    for tup in tups:
+        countries.append(tup[0])
+        cases.append(tup[1])
+        deaths.append(tup[2])
+    
+    for case in cases:
+        case = str(case)
+        if "," in case:
+            case = float(case.replace(",", ""))
+        num_cases.append(case)
+    
+    for death in deaths:
+        death = str(death)
+        if "," in death:
+            death = float(death.replace(",", ""))
+        num_deaths.append(death)
+
+    tuples = []
+    for x in range(213):
+        tuples.append((num_deaths[x], num_cases[1]))
+
+    rates = []
+    for tup in tuples:
+        rate = round((int(tup[0])/int(tup[1])), 4)
+        rates.append(rate)
+
+    num_rates = []
+    for rate in rates:
+        num_rates.append(round(rate, 4))
+
+    data = []
+    for x in range(213):
+        data.append((countries[x], num_cases[x], num_deaths[x], num_rates[x]))
+
+    with open("rates_cases_deaths.txt", "w") as output:
+        output.write("Country Name, Cases, Deaths, Rate of Deaths Per Case\n")
+        for x in data: 
+            output.write(str(x) + "\n")  
+
+#difference_in_pollution()
+#cases_density()
+#cases_difference()
+#difference_density()
+rate_cases_deaths()
