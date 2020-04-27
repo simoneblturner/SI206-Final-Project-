4fc47cd363973de3ff815f3300de0e002434c9b3
Simone Turner <sbturner@umich.edu>
2020-04-26 22:35:40 -0500

    done

--- joint.py
+++ joint.py
@@ -10,21 +10,28 @@ import Corona_Cases_Final
 import matplotlib
 import matplotlib.pyplot as plt
 
-# CREATE POPULATION TABLE
+# PART ONE CREATE ONE DATABASE FOR ALL INFORMATION
 conn = sqlite3.connect('COVID_Pollution_Correlation.db')
 cur = conn.cursor()
 
-# CREATE UNIVERSAL KEY FOR EVERY COUNTRY
+# PART TWO CREATE UNIVERSAL KEY FOR EVERY COUNTRY
 def create_database():
     
+    # TAKE LIST OF COUNTRIED FROM THREE SEPARATH FILES
+    # FILES: Population.py outputs country_list_population 
+    # FILES: Pollution.py outputs country_list_pollution
+    # FILES: Corona_Cases_Final.py outputs country_list_corona
     pollution = Pollution.country_list_pollution()
     population = Population.country_list_population()
     corona = Corona_Cases_Final.country_list_corona()
 
+    # MERGE LISTS FROM 2 APIS AND WEBSITE
+    # ONE LARGE, UNIVERSAL, SORTED LIST OF COUNTRIES WITHOUT DUPLICATES
     merged_list = population + pollution + corona
     all_countries = list(set(merged_list))
     all_countries = sorted(all_countries)
 
+    # CREATE TABLE TO ASSIGN UNIVERSAL ID TO EACH COUNTRY TO USE AS PRIMARY KEY
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS Country_ids(
@@ -33,7 +40,8 @@ def create_database():
         )
         """
     )
- 
+
+    # CHECK NUMBER OF ROWS IN Country_ids TABLE
     cur.execute(
         """ 
         SELECT COUNT (*) FROM Country_ids
@@ -41,6 +49,9 @@ def create_database():
         )
     c = cur.fetchone()[0]
     i = c
+
+    # INSERT 20 ROWS AT A TIME
+    # RUN FUNCTION MULITPLE TIMES TO GET MORE ROWS
     for country in all_countries[c:c+20]: 
         i += 1
         try:
@@ -58,10 +69,10 @@ def create_database():
         
     conn.commit()
 
-create_database()
+# PART THREE FUNCTION TO CREATE TABLE FOR POLLUTION DATA
 
-# CREATE TABLE FOR POLLUTION DATA
 def population_data():
+    # REQUEST AND GET DATA FROM API
     url = "https://ajayakv-rest-countries-v1.p.rapidapi.com/rest/v1/all"
 
     headers = {
@@ -71,8 +82,10 @@ def population_data():
 
     response = requests.request("GET", url, headers=headers)
 
+    # TURN DATA INTO JSON FILE
     data = response.json()
 
+    # CREATE POPULATION DENSITY TABLE IN COVID_Pollution_Correlation.db 
     cur.execute(
         """
         CREATE TABLE IF NOT EXISTS popul_density(
@@ -85,6 +98,9 @@ def population_data():
         )
         """
     )
+
+    # CHECK THE NUMBER OF ROWS IN popul_density
+
     cur.execute(
         """ 
         SELECT COUNT (*) FROM popul_density
@@ -93,6 +109,8 @@ def population_data():
 
     c = cur.fetchone()[0]
 
+    # INSERT 20 ROWS AT A TIME INTO popul_density TABLE FROM data
+    # ASSIGN PRIMARY ID KEY TO ALL THE COUNTRIES 
     for country in data[c:c+20]:
         country_name = country["name"]
         cur.execute("SELECT country_id FROM Country_ids WHERE country_name = ?", (country_name,))
@@ -111,12 +129,14 @@ def population_data():
 
     conn.commit()
 
-#CREATE POLLUTION TABLE 
+# PART FOUR FUNCTION TO CREATE POLLUTION TABLE 
 
 def pollution_data():
+    # REQUEST DATA FROM WEBSITE 
     r = requests.get("https://www.numbeo.com/pollution/rankings_by_country.jsp?title=2020&displayColumn=0")
     soup = BeautifulSoup(r.text, "html.parser")
 
+    # PUT POLLUTION COUNTRIES INTO LIST - 2020
     Countrynames2020 =[]
 
     Countrynames = soup.find_all("td", class_ = "cityOrCountryInIndicesTable")
@@ -124,6 +144,7 @@ def pollution_data():
         country = country.text
         Countrynames2020.append(country)
 
+    # PUT POLLUTION DATA INTO LISTS - 2020
     Pollution2020 = []
 
     Pollution = soup.find_all("td", style = "text-align: right")
@@ -134,46 +155,61 @@ def pollution_data():
     r = requests.get("https://www.numbeo.com/pollution/rankings_by_country.jsp?title=2019&displayColumn=0")
     soup = BeautifulSoup(r.text, "html.parser")
 
+    # PUT POLLUTION COUNTRIES INTO LIST - 2019
     Countrynames2019 = []
     Countrynames2 = soup.find_all("td", class_ = "cityOrCountryInIndicesTable")
     for country1 in Countrynames2:
         country1 = country1.text
         Countrynames2019.append(country1)
 
+    # PUT POLLUTION DATA INTO LIST - 2019 
     Pollution2019 = []
     Pollution2 = soup.find_all("td", style = "text-align: right")
     for num in Pollution2:
         num = num.text
         Pollution2019.append(num) 
 
+    # PUT 2019 POLLUTION DATA INTO TUPLES
     tup2019 = []
     for x in range(len(Countrynames2019)):
         tup = (Countrynames2019[x],Pollution2019[x])
         tup2019.append(tup)
 
+    # PUT 2020 POLLUTION DATA INTO TUPLES
     tup2020 =[]
     for x in range(len(Countrynames2020)):
         if Countrynames2020[x] in Countrynames2019:
             tup = (Countrynames2020[x],Pollution2020[x])
             tup2020.append(tup)
 
+    # SORT TUPLES
     tup2019 = sorted(tup2019)
     tup2020 = sorted(tup2020)
 
+    # CREATE 2019 POLLUTION TABLE
     cur.execute("CREATE TABLE IF NOT EXISTS pollution2019 (id INTEGER PRIMARY KEY, country STRING, pollution_index FLOAT)")
+    
+    # CHECK NUMBER OF ROWS IN POLLUTION 2019 TABLE
     cur.execute(
         """ 
         SELECT COUNT (*) FROM pollution2019
         """
     )
     c = cur.fetchone()[0]
+
+    # INSERT 20 ITEMS INTO TABLE AT A TIME 
+    # RUN AS MANY TIMES TO GATHER AND INSERT ALL OF THE INFORMATION 
     for i in tup2019[c:c+20]:
+        # MATCH UNIVERSAL COUNTRY ID TO COUNTRY IN POLLUTION DATA 
         cur.execute("SELECT country_id FROM Country_ids WHERE country_name = ?", (i[0],))
         name = cur.fetchone()[0]
         cur.execute("INSERT INTO pollution2019 (id,country, pollution_index) VALUES (?, ?, ?)",(name, i[0], i[1]))
     conn.commit()
 
+    # CREATE TABLE FOR POLLUTION 2020 
     cur.execute("CREATE TABLE IF NOT EXISTS pollution2020 (id INTEGER PRIMARY KEY, country STRING, pollution_index FLOAT)")
+    
+    # CHECK HOW MANY ROWS ARE IN pollution2020 
     cur.execute(
         """ 
         SELECT COUNT (*) FROM pollution2020
@@ -182,25 +218,33 @@ def pollution_data():
 
     con = cur.fetchone()[0]
     
+    # INSERT 20 ITEMS INTO TABLE AT A TIME 
+    # RUN AS MANY TIMES TO GATHER AND INSERT ALL OF THE INFORMATION
     for i in tup2020[con:con+20]:
+        # MATCH UNIVERSAL COUNTRY ID TO COUNTRY IN POLLUTION DATA 
         cur.execute("SELECT country_id FROM Country_ids WHERE country_name = ?", (i[0],))
         name = cur.fetchone()[0]
         cur.execute("INSERT INTO pollution2020 (id, country, pollution_index) VALUES (?,?,?)",(name, i[0],i[1]))
     conn.commit()
 
-# CREATE COVID CASES TABLE
+# PART FIVE FUNCRION TO CREATE COVID CASES TABLE
 
 def covid_case_data():
+    # GET DATA FROM CORONA API
     url = "https://coronavirus-monitor-v2.p.rapidapi.com/coronavirus/cases_by_country.php"
     headers = {
         'x-rapidapi-host': "coronavirus-monitor-v2.p.rapidapi.com",
         'x-rapidapi-key': "0703ba965emsheeeaf0cc17ceeeep1f13dbjsnf8a169011985"}
 
     response = requests.request("GET", url, headers=headers)
+    
+    # PUT DATA INTO JSON FORMAT 
     data = response.json()
 
+    # CREATE Cases TABLE 
     cur.execute("""CREATE TABLE IF NOT EXISTS Cases(country_id PRIMARY KEY, country_name STRING, cases INTEGER, deaths INTEGER)""")
     
+    # CHECK HOW MANY ROWS ARE IN Cases
     cur.execute(
         """ 
         SELECT COUNT (*) FROM Cases
@@ -209,8 +253,11 @@ def covid_case_data():
 
     c = cur.fetchone()[0]
 
+    # INSERT 20 ROWS INTO Cases FROM COVID-19 DATA
+    # RUN THIS FUNCTION AS MANY TIMES TO GET AND INSERT ALL OF THE DATA
     for country in data["countries_stat"][c:c+20]:
         country_name = country["country_name"]
+        # MATCH IDS FOR COUNTRIES BETWEEN EACH TABLE
         cur.execute("SELECT country_id FROM Country_ids WHERE country_name = ?", (country_name,))
         name = cur.fetchone()[0]
         cur.execute(
@@ -222,6 +269,7 @@ def covid_case_data():
     
     conn.commit() 
 
+# CHECK HOW MANY ROWS ARE IN Country_ids
 cur.execute(
         """ 
         SELECT COUNT (*) FROM Country_ids
@@ -229,29 +277,41 @@ cur.execute(
     )
 one = cur.fetchone()[0]
 
+# RUN THE FIRST FUNCTION UNTIL ALL THE DATA IS IN THE DATABASE/TABLE
 if one == 281:
+    # START RUNNING SECOND FUNCTION
     population_data()
+    # CHECK HOW MANY ROWS ARE IN popul_density FROM THE SECOND FUNCTION 
     cur.execute(
             """ 
             SELECT COUNT (*) FROM popul_density
             """
         )
     two = cur.fetchone()[0]
-
+    # RUN UNTIL popul_density TABLE HAS ALL 240 DATA ROWS 
     if two == 240:
+        #START RUNNING THIRD FUNCTION 
         pollution_data()
+        # CHECK HOW MANY ROWS ARE IN pollution 2019 FROM THE THIRD FUNCTION 
         cur.execute(
             """ 
             SELECT COUNT (*) FROM pollution2019
             """
             )
         three = cur.fetchone()[0]
-
+        # RUN UNTIL pollution2019 AND pollution2020 TABLES HAVE ALL THE DATA INSERTED 
         if three == 106:
+            # START RUNNING FOURTH FUNCTION 
             covid_case_data()
+            # FOURTH FUNCTION MAKES THE Cases TABLE - CHECK HOW MANY ROWS ARE IN THE TABLE 
             cur.execute(
                 """ 
                 SELECT COUNT (*) FROM Cases
                 """
                 )
             four = cur.fetchone()[0]
+
+# THERE IS SO MUCH DATA FROM OUR THREE SOURCES GOING INTO ONE DATABASE
+# WE CAN ONLY DO IT 20 ITEMS AT A TIME 
+# IN ORDER TO INSERT ALL THE DATA YOU MUST RUN THE FILE MANY TIMES 
+# ONCE THE DATABASE IS CREATED AND FULL, CAN USE TO RUN VISUALIZATIONS 
